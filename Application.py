89128227fb15d1fe613d0b227b0be2270dd8684e
Giovanna Pellegrini <giovannapellegrini0602@gmail.com>
2025-11-13 21:36:42 -0300

    Organização de pastas

--- Application.py
+++ Application.py
@@ -1,84 +1,52 @@
-import os
+from model.cliente import Cliente
+from model.veiculo import Veiculo
+from service.cliente_service import cadastro_clientes,lista_clientes
 
-from cliente import Cliente
-
-from veiculo import Veiculo
-
-import layout 
-
-import veiculo_service
-
-
-####### VARIÁVEIS GLOBAIS
-clientes = []
-
-def lista_clientes():
-    print("\n#LISTA DE CLIENTES")
-    print("-"*80)
-    print(f"| {'Código':^8} | {'Nome':^20} | {'Ano de Nasc.':^15} | {'E-mail':^25} |")
-    print("-"*80)
-    for cliente in clientes:
-        print(f"| {cliente.codigo:^8} | {cliente.nome:<20} | {cliente.ano_nascimento:^15} | {cliente.email:<25} |")
-    print("-"*80)
-
-
-#funções de cadastro de clientes
-def cadastro_clientes():
-
-    codigo_cliente = int(input("Código do cliente: "))
-    nome = input("Nome: ")
-    ano_nascimento = int(input("Ano de nascimento: "))
-    email = input("E-mail: ")
-
-    cliente = Cliente(codigo_cliente, nome, ano_nascimento,email)
-    clientes.append(cliente)
-    
-    print("\nCliente cadastrado com sucesso!")
-    
-
+import service.layout_service as layout_service 
+import service.veiculo_service as veiculo_service
 
 # execução do sistema
 while True:
 
-    layout.limpar_tela()
-    layout.titulo()
-    layout.menu()
+    layout_service.limpar_tela()
+    layout_service.titulo()
+    layout_service.menu()
 
     opcao=int(input("\nInforme uma opção: "))
 
     match opcao:
         case 1:
-            layout.titulo()
+            layout_service.titulo()
             print("\n#### CADASTRO DE CLIENTES ####")
             cadastro_clientes()
-            layout.pressione_continuar()
+            layout_service.pressione_continuar()
 
         case 2:
-            layout.titulo()
+            layout_service.titulo()
             lista_clientes()
-            layout.pressione_continuar()
+            layout_service.pressione_continuar()
 
         case 3:
-            layout.titulo()
+            layout_service.titulo()
             print("\n#CADASTRO DE VEÍCULOS")
             veiculo_service.cadastro_veiculos()   
-            layout.pressione_continuar()
+            layout_service.pressione_continuar()
         
         case 4: 
-            layout.titulo()
+            layout_service.titulo()
             veiculo_service.lista_veiculos()
-            layout.pressione_continuar()
+            layout_service.pressione_continuar()
 
         case 5:
             print("\n#ALUGAR CARRO")
-            layout.pressione_continuar()
+            layout_service.pressione_continuar()
 
         case 6:
             print("\n#SAIR")
             
         case _:
             print("\n#OPÇÃO INVÁLIDA")
-            layout.pressione_continuar()
+            layout_service.pressione_continuar()
 
 
 
--- test_import.py
+++
@@ -1,6 +0,0 @@
-from cliente import Cliente
-
-print("Import OK!")
-
-c = Cliente(1, "João", 1990, "joao@email.com")
-print(c)
